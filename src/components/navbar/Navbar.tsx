94f7ae26a1f7ddec0f74c4370d9a461f866ac082
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-11 14:33:00 +0100

    chore: map over navlinks for Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
+import { navLinks } from "@/src/constants";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link href="/">Navbar</Link>
+      <div>
+        {navLinks.map((link) => (
+          <Link href={link.url} key={link.id}>{link.title}</Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
