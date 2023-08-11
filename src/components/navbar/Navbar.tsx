31ff050788127e571ec0634b1e832538b5e79545
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-11 15:22:53 +0100

    feat: Navbar nav links (inc. active style)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,36 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { navLinks } from "@/src/constants";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
-    <div>
-      <Link href="/">Navbar</Link>
-      <div>
-        {navLinks.map((link) => (
-          <Link href={link.url} key={link.id}>{link.title}</Link>
-        ))}
+    <div className="h-[100px] flex justify-between items-center">
+      <Link href="/" className="font-bold text-[22px]">
+        Navbar
+      </Link>
+      <div className="flex items-center gap-5">
+        {navLinks.map((link) => {
+          const isActive =
+            (link.url.length > 1 && pathname.includes(link.url)) ||
+            pathname === link.url;
+          return (
+            <Link
+              href={link.url}
+              className={isActive ? "text-white" : "text-gray-400"}
+              key={link.id}
+            >
+              {link.title}
+            </Link>
+          );
+        })}
+        <button className="p-1 border-none bg-[#53c28b] text-white text-sm cursor-pointer rounded-sm hover:scale-110 transition">
+          Logout
+        </button>
       </div>
     </div>
   );
