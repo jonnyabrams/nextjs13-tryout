53ef4df756d8cb503aaeb2d864196586d332da35
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-11 15:39:25 +0100

    feat: Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <div>
+    <div className="h-[50px] flex items-center justify-between text-sm">
       <div>© Jonny. All rights reserved.</div>
-      <div></div>
+      <div className="flex items-center gap-2.5">
+        <Image src="/assets/1.png" alt="" width={15} height={15} className="opacity-60 cursor-pointer" />
+        <Image src="/assets/2.png" alt="" width={15} height={15} className="opacity-60 cursor-pointer" />
+        <Image src="/assets/3.png" alt="" width={15} height={15} className="opacity-60 cursor-pointer" />
+        <Image src="/assets/4.png" alt="" width={15} height={15} className="opacity-60 cursor-pointer" />
+      </div>
     </div>
   );
 };
