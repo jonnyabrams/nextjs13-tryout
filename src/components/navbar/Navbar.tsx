7cc786638b2635a3e2f060473894099eedf28956
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-12 14:59:44 +0100

    feat: menu dropdown for smaller screens

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,8 +5,10 @@ import { usePathname } from "next/navigation";
 import { ImMenu3 } from "react-icons/im";
 
 import { navLinks } from "@/src/constants";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
   const pathname = usePathname();
 
   return (
@@ -33,8 +35,34 @@ const Navbar = () => {
           Logout
         </button>
       </div>
-      <div className="md:hidden">
+      <div
+        onClick={() => setShowDropdown(!showDropdown)}
+        className="md:hidden relative"
+      >
         <ImMenu3 style={{ color: "white", fontSize: "40" }} />
+        {showDropdown && (
+          <div className="absolute right-3.5 top-9 min-w-[160px] min-h-[280px] py-2 rounded-sm bg-white">
+            <div className="flex flex-col items-center gap-5">
+              {navLinks.map((link) => {
+                const isActive =
+                  (link.url.length > 1 && pathname.includes(link.url)) ||
+                  pathname === link.url;
+                return (
+                  <Link
+                    href={link.url}
+                    className={isActive ? "text-black" : "text-gray-400"}
+                    key={link.id}
+                  >
+                    {link.title}
+                  </Link>
+                );
+              })}
+              <button className="p-1 border-none bg-[#53c28b] text-white text-sm cursor-pointer rounded-sm hover:scale-110 transition">
+                Logout
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
