8feb7dc8e6003f5d73f18e2ed129762661779983
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-13 20:19:47 +0100

    feat: dashboard fetches logged in user's posts only

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -4,10 +4,21 @@ import connect from "@/src/utils/db";
 import Post from "@/src/models/Post";
 
 export const GET = async (req: NextRequest) => {
+  const url = new URL(req.url);
+
+  const username = url.searchParams.get("username");
+
   try {
     await connect();
 
-    const posts = await Post.find();
+    // for ts - this way, eventual query for Post.find can be empty or a username
+    let query: any = {};
+
+    if (username) {
+      query = { username };
+    }
+
+    const posts = await Post.find(query);
 
     return new NextResponse(JSON.stringify(posts), { status: 200 });
   } catch (error) {
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,17 +2,34 @@
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import useSWR from "swr";
 
 const Dashboard = () => {
   const session = useSession();
   const router = useRouter();
+
+  // const fetcher = (...args: any) => fetch(...args).then((res) => res.json());
+
+  // ts way?
+  const fetcher = async (...args: Parameters<typeof fetch>) => {
+    const res = await fetch(...args);
+    const data = await res.json();
+    return data;
+  };
+
+  const { data, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user?.name}`,
+    fetcher
+  );
+
+  console.log(data)
 
   if (session.status === "loading") {
     return <p>Loading...</p>;
   }
 
   if (session.status === "unauthenticated") {
-    router?.push("/dashboard/login")
+    router?.push("/dashboard/login");
   }
 
   if (session.status === "authenticated") {
