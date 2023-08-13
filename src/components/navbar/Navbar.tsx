9fe81849bd800cb42cacad91c6bb0578bdb300c3
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-13 20:40:47 +0100

    styles: redesign dropdown

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = () => {
       >
         <ImMenu3 style={{ color: "white", fontSize: "40" }} />
         {showDropdown && (
-          <div className="absolute right-3.5 top-9 min-w-[160px] py-3 rounded-sm bg-gray-800">
+          <div className="absolute right-3.5 top-9 min-w-[160px] py-3 rounded-sm bg-black border-2 border-white">
             <div className="flex flex-col items-center gap-2">
               {navLinks.map((link) => {
                 const isActive =
@@ -65,7 +65,7 @@ const Navbar = () => {
                   <Link
                     href={link.url}
                     className={`${
-                      isActive ? "font-semibold" : "font-light"
+                      isActive ? "font-bold" : "font-extralight"
                     } w-full text-white text-center hover:bg-gray-400 transition`}
                     key={link.id}
                   >
@@ -76,7 +76,7 @@ const Navbar = () => {
               {session.status === "authenticated" && (
                 <button
                   onClick={handleLogout}
-                  className="p-1 mt-5 border-none bg-primary text-white text-sm cursor-pointer rounded-sm hover:scale-110 transition"
+                  className="p-1 mt-5 border-none bg-primary text-white text-sm cursor-pointer rounded-sm hover:bg-green-700 transition duration-300"
                 >
                   Logout
                 </button>
